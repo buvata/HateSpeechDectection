459525459914066c765788d1b4ab82155960ba37
trangtv57 <truongtrang.1459@gmail.com>
2019-09-21 23:19:48 +0700

    add eda function gen offensive and hate data

--- module_dataset/preprocess_data/eda_data.py
+++ module_dataset/preprocess_data/eda_data.py
@@ -2,7 +2,6 @@ from module_dataset.preprocess_data.handle_text import *
 from module_train.ml_model.get_important_kw import *
 
 from utilities import *
-from pyvi import ViTokenizer
 
 
 def get_important_kw(path_data_train, path_save_important_kw, n_kw_extract=50):
@@ -21,8 +20,27 @@ def get_important_kw(path_data_train, path_save_important_kw, n_kw_extract=50):
     get_feature_importance(list_train_data, list_label, path_save_important_kw, n_kw_extract)
 
 
+def get_data_type_1_2(path_dataset, path_offensive, path_hate):
+    w_offensive = open(path_offensive, "w")
+    w_hate = open(path_hate, "w")
+    with open(path_dataset, "r") as rf:
+        for e_line in rf.readlines():
+            e_line = e_line.replace("\n", "")
+            text_data = e_line.split("|")[0]
+            label = int(e_line.split("|")[1])
+            if label == 1:
+                w_offensive.write(text_data + "\n")
+            if label == 2:
+                w_hate.write(text_data + "\n")
+
+    w_offensive.close()
+    w_hate.close()
+
 if __name__ == '__main__':
     path_config = "/home/trangtv/Documents/project/HateSpeechDectection/module_dataset/preprocess_data/" \
                   "config_dataset.json"
     cf = load_config(path_config)
-    get_important_kw(cf['train_process_emoji_punct'], cf['path_save_important_kw'], 200)
+    # get_important_kw(cf['train_process_emoji_punct'], cf['path_save_important_kw'], 200)
+    get_data_type_1_2(cf['train_process_emoji_punct'],
+                      cf['path_offensive_data'],
+                      cf['path_hate_data'])
